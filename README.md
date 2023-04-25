ee6e8e379df71316169e23e65df2e18d395c8a2a
Natalia <bgutisol2019@gmail.com>
2023-04-25 17:08:49 -0400

    merge?

--- README.md
+++
@@ -1,2 +0,0 @@
-# FERIA-DEL-EMPLEO-UCB
-El sitio web de la Feria del Empleo está destinada a la interacción entre sus participantes: instituciones, estudiantes y/o graduados UCB, emprendimientos de graduados UCB, autoridades y administrativos
--- back_end/src/routes.js
+++
@@ -1,8 +0,0 @@
-const AuthenticationController = require('./controllers/AuthenticationController')
-const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
-
-module.exports = (app) => {
-    app.post('/login', 
-        AuthenticationControllerPolicy.login,
-        AuthenticationController.login)
-}
