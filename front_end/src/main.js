5680215d1d848be2efd1792b5cc809d2ca67ea80
jp <jpv.9pm@gmail.com>
2023-04-29 01:43:22 -0400

    calendario v1

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -2,8 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueTextareaAutosize from 'vue-textarea-autosize'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+
+Vue.use(VueTextareaAutosize); // VUE TEXT AREA INITIALIZER
 
 Vue.config.productionTip = false
+
+// FIREBASE CONNECTION TO GOOGLE FIREBASE 
+firebase.initializeApp({
+  apiKey: "AIzaSyANS-PYg8rd4L7KTs1vZ8QqcmYSWLUZML4",
+  authDomain: "vue-calendar-6b39f.firebaseapp.com",
+  projectId: "vue-calendar-6b39f",
+  storageBucket: "vue-calendar-6b39f.appspot.com",
+  messagingSenderId: "576839342965",
+  appId: "1:576839342965:web:9a108b3b1c2b42300ed13c"
+});
+
+export const db = firebase.firestore(); // FIREBASE DATABASE
+
+
 
 new Vue({
   router,
