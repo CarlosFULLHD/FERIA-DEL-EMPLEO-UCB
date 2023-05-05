817b6f407a523b80f3788964dda34bd2d562c385
Natalia <bgutisol2019@gmail.com>
2023-05-05 05:09:40 -0400

    home diseño 1

--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -2,18 +2,31 @@
   <div class="home">
     <ApiTest/>
     <section class="hero">
-      <div class="hero-text container">
-        <h4>Vue Responsive Nav Menu</h4>
-        <hr />
-        <h2>
-          With Vue <br />
-          Animations
-        </h2>
-        <hr />
-      </div>
-    </section>
-    <section>
-     
+      <v-container class="bg-surface-variant mb-6">
+        <v-row align="start" no-gutters style="height: 150px;">
+          <v-col>
+            <v-row align="start" no-gutters style="height: auto;">
+              <h1 style="font-family: Roboto, ">Feria de Empleo</h1>
+            </v-row>
+            <v-row align="center" no-gutters style="height: auto;">
+              <p style="font-family: Roboto, ">La Feria del Empleo UCB es un evento que concentra la oferta y demanda de puestos laborales, facilita el contacto entre instituciones empleadoras y personas interesadas en obtener un trabajo.</p>
+            </v-row>
+          </v-col>
+          <v-col>
+            <!--<v-sheet class="pa-2 ma-2">
+              .align-start
+            </v-sheet>-->
+            <v-carousel hide-delimiters>
+              <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+                cover
+              ></v-carousel-item>
+            </v-carousel>
+          </v-col>
+        </v-row>
+      </v-container>
     </section>
   </div>
 </template>
@@ -21,17 +34,41 @@
 <script>
 // @ is an alias to /src
 import ApiTest from "@/components/TarjetasEmpresas.vue";
+
 export default {
   name: "HomeView",
   components: {
     ApiTest,
+  },
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .hero {
-  background-image: url("../assets/hero-bg.png");
+  background-image: url("../assets/cato-foto.jpg");
   background-attachment: fixed;
   position: relative;
   height: 100vh;
