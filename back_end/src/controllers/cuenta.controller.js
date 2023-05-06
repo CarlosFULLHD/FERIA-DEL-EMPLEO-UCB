f341dbeb5c0bf0a8343988cd7abe4cd980ab62f8
CarlosFULLHD <cninareynaga@gmail.com>
2023-05-05 22:02:22 -0400

    ?,

--- back_end/src/controllers/cuenta.controller.js
+++ back_end/src/controllers/cuenta.controller.js
@@ -10,7 +10,7 @@ export const createCuenta = async (req, res) => {
       const {cuenta,pwd,superusuario,email} = req.body;
       const [rows] = await pool.query(
         
-        "INSERT INTO instituciones(cuenta_id, cuenta,pwd,superusuario,email) VALUES (NULL,?, ?, ?, ?, ?)",
+        "INSERT INTO instituciones(cuenta_id, cuenta,pwd,superusuario,email) VALUES (NULL, ?, ?, ?, ?)",
         [cuenta,pwd,superusuario,email]
       );
       res.status(201).json({
