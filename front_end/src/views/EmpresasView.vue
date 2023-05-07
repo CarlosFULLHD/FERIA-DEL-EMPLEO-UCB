5daa0ce9312615dbe16352922422c5f7f2087390
willyv090 <willyjesus090@gmail.com>
2023-05-07 16:15:36 -0400

    tarjetas

--- front_end/src/views/EmpresasView.vue
+++ front_end/src/views/EmpresasView.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main style="background-image: linear-gradient(to right ,#b2cde4, #5684d4 ) ;"
+    >
       <v-container>
         <v-row 
         justify="center"
@@ -15,14 +16,14 @@
           >
           v-for
           <v-hover v-slot="{ hover }">
-            <div class="card">
+            <div class="card" >
             <v-card
               :elevation="hover ? 12 : 2"
               :class="{ 'on-hover': hover }"
               
               max-width="350"
               class="my-3"
-              
+              color="pink"
             >
             <v-carousel hide-delimiters>
             <v-carousel-item
@@ -35,14 +36,44 @@
               <v-card-subtitle class="pt-4">
                 Algo extra
               </v-card-subtitle>
-              <v-card-text>
+              <v-card-text >
                 <div>Banco</div>
                 <div>Informacion del Banco</div>
               </v-card-text>
               <v-card-actions>
-                <v-btn color="#ffc506">
-                  Opcion
+
+                <v-btn v-on:click="showPopup = true"
+                    class="mx-2"
+                    fab
+                    dark
+                    large
+                    color="blue"
+                    elevation="24"
+                  >
+                  
+                  <v-icon dark
+                  color="yellow"
+                  >
+                    mdi-pencil
+                  </v-icon>
                 </v-btn>
+                <v-dialog v-model="showPopup">
+                  <v-card>
+                    <v-card-title>{{ titulo }}</v-card-title>
+                    <v-card-text>
+                      {{ contenido }}
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn color="secondary" @click="showPopup = false">Cerrar</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+
+
+
+
+
                 <v-btn color="#ffc506">
                   <button id="miBoton" class="miBoton">Información</button>
                 </v-btn>
@@ -59,6 +90,11 @@
   <script>
     export default {
       data: () => ({
+      
+          showPopup: false,
+          titulo: 'Título de la carta',
+          contenido: 'Aquí va el contenido de la carta',
+       
         items: [
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
