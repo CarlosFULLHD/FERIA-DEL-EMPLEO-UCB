3ce349e4985fecdec07c2d56348b0a46a182f835
CarlosFULLHD <cninareynaga@gmail.com>
2023-05-10 16:36:39 -0400

    login, crearcuenta changes

--- front_end/src/views/CrearCuentaView.vue
+++ front_end/src/views/CrearCuentaView.vue
@@ -1,117 +1,82 @@
 
 <template>
+  <v-app>
+    <v-sheet width="500" class="mx-auto grey lighten-3 py-6 px-8">
+      <h1 class="mb-6">Crear cuenta</h1>
+      <v-form ref="form" @submit.prevent="enviarCuenta">
+        <v-text-field
+          v-model="cuenta"
+          hint="Nombre de cuenta"
+          prepend-icon="mdi mdi-account-circle-outline"
+          :rules="[rules.required, rules.min]"
+          label="Cuenta"
+          autofocus="true"
+          clearable
+          required
+        ></v-text-field>
 
-    <v-app>
-        <h1>HOLA</h1>
-      <v-sheet width="300" class="mx-auto">
-        <h1>Creación de cuenta</h1>
-        <v-form ref="form" @submit.prevent="enviarCuenta">
-          <v-text-field
-            v-model="cuenta"
-            hint="Nombre de cuenta"
-            prepend-icon="mdi mdi-account-circle-outline"
-            :rules="[rules.required, rules.min]"
-            label="Cuenta"
-            autofocus="true"
-            clearable
-            required
-          >
-          </v-text-field>
-    
-          <v-text-field
-            v-model="password"
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
-            prepend-icon="mdi mdi-lock-outline"
-            hint="Al menos 8 caracteres "
-            label="Contraseña"
-            clearable
-            @click:append="show1 = !show1"
-          ></v-text-field>
-            
-          <v-text-field
-            v-model="email"
-            prepend-icon="mdi mdi-email-outline"
-            :rules="[rules.required, rules.email]"
-            label="E-mail"
-            clearable
-          >
-    
-            
-          </v-text-field>
-    
-    
-          <v-checkbox
-            v-model="checkbox"
-            :rules="[v => !!v || '¡Debes aceptar los términos para continuar!']"
-            required
-            color="green">
+        <v-text-field
+          v-model="password"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]"
+          :type="show1 ? 'text' : 'password'"
+          prepend-icon="mdi mdi-lock-outline"
+          hint="Al menos 8 caracteres "
+          label="Contraseña"
+          clearable
+          @click:append="show1 = !show1"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email"
+          prepend-icon="mdi mdi-email-outline"
+          :rules="[rules.required, rules.email]"
+          label="E-mail"
+          clearable
+        ></v-text-field>
+
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[v => !!v || '¡Debes aceptar los términos para continuar!']"
+          required
+          color="green"
+        >
           <template v-slot:label>
-                  <div @click.stop="">
-                    ¿Super usuario?
-                    
-                  </div>
+            <div @click.stop="">¿Super usuario?</div>
           </template>
-    
-    
-          </v-checkbox>
-    
-    
-    
-          <v-btn
-            color="success"
-            class="mr-4"
-            @click="submitForm"
-          >
-            Validate
-          </v-btn>
-      
-          <v-btn
-            color="error"
-            class="mr-4"
-            @click="reset"
-          >
-            Reset Form
-          </v-btn>
-      
-          <v-btn
-            color="warning"
-            @click="resetValidation"
-          >
-            Reset Validation
-          </v-btn>
-    
-    
-    
-          <v-btn
-          :disabled="!formIsValid"
-          text
-          color="primary"
-          type="submit"
-          >
-          Crear cuenta
-          </v-btn>
-        </v-form>
-      </v-sheet>
-    </v-app>
-    
-    </template>
-      
-      <style>
-      @media (min-width: 1024px) {
-        .login {
-          min-height: 100vh;
-          display: flex;
-          align-items: center;
-          
-        }
-        
-      }
-      .error {
-          color:red;
-        }
-      </style>
+        </v-checkbox>
+
+        <div class="mt-6">
+          <v-btn color="primary" :disabled="!formIsValid" type="submit" @click="submitForm">Crear cuenta</v-btn>
+          <!-- <v-btn color="error" class="ml-4" @click="reset">Reset Form</v-btn> -->
+          <!-- Deshabilidato el button -->
+        <h4 class="achecuatro">Ya tienes una cuenta? Haz click  <router-link to="/cuenta/login">aqui</router-link></h4>
+        </div>
+      </v-form>
+    </v-sheet>
+  </v-app>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+.error {
+  color: red;
+}
+.achecuatro{
+  color: rgb(85, 83, 83);
+}
+.v-application--wrap {
+  height: 100%;
+  background-color: #f5f5f5;
+}
+</style>
     
     <script>
 
@@ -136,9 +101,9 @@
                 rules: {
                     required: v => !!v || 'Requerido.',
                     min: v => v.length >= 8 || 'Mínimo 8 caracteres',
-                    confirmPassword: [
+                   /* confirmPassword: [
                       (val) => val === this.password || 'Passwords do not match',
-                      (val) => (val || '').length > 0 || 'Confirm password is required',],
+                      (val) => (val || '').length > 0 || 'Confirm password is required',],*/
                     email: value => {
                     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                     return pattern.test(value) || 'e - mail inválido'},
--- front_end/src/views/LoginView.vue
+++ front_end/src/views/LoginView.vue
@@ -1,136 +1,98 @@
 <template>
-
   <v-app>
-    <v-sheet width="300" class="mx-auto">
-      <h1>Iniciar Sesion</h1>
-      <v-form ref="form" @submit.prevent="loginForm">
-        <v-text-field
-          v-model="cuenta"
-          hint="Nombre de cuenta"
-          prepend-icon="mdi mdi-account-circle-outline"
-          :rules="[rules.required, rules.min]"
-          label="Cuenta"
-          autofocus="true"
-          clearable
-          required
-        >
-        </v-text-field>
-  
-        <v-text-field
-          v-model="password"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
-          :type="show1 ? 'text' : 'password'"
-          prepend-icon="mdi mdi-lock-outline"
-          hint="Al menos 8 caracteres "
-          label="Contraseña"
-          clearable
-          @click:append="show1 = !show1"
-        ></v-text-field>
+    <v-container fluid>
+      <v-row justify="center" align="center" class="mt-10">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card class="cambioscss">
+            <v-card-title class="text-center">Iniciar sesión</v-card-title>
+            <v-card-text>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                  v-model="cuenta"
+                  :rules="[rules.required, rules.min]"
+                  label="Nombre de cuenta"
+                  prepend-icon="mdi mdi-account-circle-outline"
+                  outlined
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :rules="[rules.required, rules.min]"
+                  label="Contraseña"
+                  prepend-icon="mdi mdi-lock-outline"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="showPassword ? 'text' : 'password'"
+                  outlined
+                  required
+                  @click:append="showPassword = !showPassword"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" @click="loginForm">Iniciar sesión</v-btn>
+              <v-btn color="secondary" @click="resetForm">Limpiar</v-btn>
+            </v-card-actions>
+            <h4 class="achecuatro">No tienes una cuenta? Haz click  <router-link to="/cuenta/crear">aqui</router-link></h4>
+          </v-card>
+        </v-col>
+      </v-row>
       
-        <v-btn
-          color="error"
-          class="mr-4"
-          @click="reset"
-        >
-          Limpiar
-        </v-btn>
-  
-        <v-btn
-          color="success"
-          class="mr-4"
-          @click="loginForm"
-        >
-          Login
-        </v-btn>
-    
-      </v-form>
-    </v-sheet>
+    </v-container>
   </v-app>
-  
-  </template>
-    
-    <style>
-    @media (min-width: 1024px) {
-      .login {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-        
+</template>
+
+<script>
+import Cuentas from '@/services/Cuenta';
+
+export default {
+  data() {
+    return {
+      cuenta: '',
+      password: '',
+      showPassword: false,
+      valid: false,
+      rules: {
+        required: value => !!value || 'Requerido.',
+        min: value => (value && value.length >= 8) || 'Mínimo 8 caracteres',
+      },
+    };
+  },
+  methods: {
+    async loginForm() {
+      try {
+        const usuario = await Cuentas.loginCuenta(this.cuenta, this.password);
+        const data = usuario.data;
+        if (data.length !== 0) {
+          this.$store.dispatch('changeUserAccount', data.cuenta);
+          this.$store.dispatch('changeUserPwd', data.pwd);
+          this.$store.dispatch('changeUserEmail', data.email);
+          this.$store.dispatch('changeSuperUser', data.superusuario);
+          this.$store.dispatch('changeUserId', data.cuenta_id);
+          this.$store.dispatch('changeloggedinFlag', true);
+          this.$store.dispatch('successAlertAsync', `Bienvenido ${data.cuenta}`);
+          //Para redireccionar a Home despues de verificar el inicio de sesion
+          this.$router.push({ name: 'Home' });
+        }
+      } catch (error) {
+        this.$store.dispatch('errorAlertAsync', 'Cuenta inexistente, intente de nuevo');
       }
-      
-    }
-    .error {
-        color:red;
-      }
-    </style>
-  
-  <script>
-  import Cuentas from '@/services/Cuenta'
-  export default {
-    name:"LoginView",
-      data () {
-        const defaultForm = Object.freeze({
-          account:'',
-          password: '',
-          confpassword: '',
-          email:'',
-          error: null,
-          show1: false,
-          show2: false,
-          valid: true,
-          terms: false,
-        })
-        
-          return {
-              form: Object.assign({}, defaultForm),
-              rules: {
-                  required: v => !!v || 'Requerido.',
-                  min: v => v.length >= 8 || 'Mínimo 8 caracteres',
-                  confirmPassword: [
-                    (val) => val === this.password || 'Passwords do not match',
-                    (val) => (val || '').length > 0 || 'Confirm password is required',],
-                  email: value => {
-                  const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                  return pattern.test(value) || 'e - mail inválido'},
-                  
-            },
-          }
-      },
-    
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
-      async loginForm(){
-        try { 
-          let usuario = await Cuentas.loginCuenta(this.cuenta,this.password)
-          let xd = usuario.data
-          if (xd.length !== 0){
-            console.log("CUENTITA")
-            
-            // this.$store.state.cuentaU = xd.cuenta
-            // this.$store.state.passwordU = xd.pwd
-            // this.$store.state.superU = xd.email
-            // this.$store.state.emailU = xd.superusuario
-            this.$store.dispatch('changeUserAccount',xd.cuenta)
-            this.$store.dispatch('changeUserPwd',xd.pwd)
-            this.$store.dispatch('changeUserEmail',xd.email)
-            this.$store.dispatch('changeSuperUser',xd.superusuario)
-            this.$store.dispatch('changeUserId',xd.cuenta_id)
-            this.$store.dispatch('changeloggedinFlag',true)
-            this.$store.dispatch('successAlertAsync',`Bienvenido ${xd.cuenta}`)
-          }
-        } catch (error) {
-          this.$store.dispatch('errorAlertAsync',`Cuenta inexistente, intente de nuevo`)
-        }     
-      },
     },
-  }
-  </script>
+    resetForm() {
+      this.$refs.form.resetValidation();
+      this.cuenta = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-card {
+  max-width: 500px;
+  margin: 0 auto;
+}
+h4{
+  padding-left: 30px;
+  padding-bottom: 20px;
+}
+</style>
